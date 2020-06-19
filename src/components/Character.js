49b361283e98fb81ed1df6e80349616053873b70
hishamss <hishamss90@gmail.com>
2020-06-19 01:40:07 -0700

    added character images in columns

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,10 +1,18 @@
 import React from "react";
-
+const styles = {
+  imgStyle: {
+    width: "200px",
+    height: "200px",
+    padding: "10px",
+    background: "lightgrey",
+    borderRadius: "10px",
+  },
+};
 function Character(props) {
   return (
-    <div className="card">
+    <div className="col-md-3 col-lg-3 col-xm-12 col-sm-6 text-center">
       <img
-        className="card-img-top"
+        style={styles.imgStyle}
         alt="characterImg"
         src={`${process.env.PUBLIC_URL}${props.bg}`}
       ></img>
--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,12 +1,8 @@
 import React from "react";
-const styles = {
-  jumb: {
-    backgroundImage: `url(${process.env.PUBLIC_URL}/assets/images/img1.jpg)`,
-  },
-};
+
 function Jumbotron() {
   return (
-    <div className="jumbotron text-center" style={styles.jumb}>
+    <div className="jumbotron text-center">
       <h1 className="display-2">Click an image to begin!</h1>
     </div>
   );
